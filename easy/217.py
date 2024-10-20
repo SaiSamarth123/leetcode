cd4cf5dc732d999760f0f80e1123863e3a1ecf4c
SaiSamarth123 <sai003sam@gmail.com>
2024-10-20 10:36:10 -0500

    problem 1

---
+++ easy/217.py
@@ -0,0 +1,14 @@
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        mySet = set(nums)
+
+        if len(mySet) != len(nums):
+            return True
+
+        return False
