30ef8b51c4477cdf4e45532c4eb4716b1f40f515
SaiSamarth123 <sai003sam@gmail.com>
2024-10-23 18:29:02 -0500

    problem 7

--- easy/206.py
+++ easy/206.py
@@ -12,6 +12,21 @@ class ListNode:
         self.next = next
 
 
+# recursive solution
+# class Solution:
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+#         if head is None or head.next is None:
+#             return head
+
+#         new_head = self.reverseList(head.next)
+
+#         head.next.next = head
+#         head.next = None
+
+#         return new_head
+
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
@@ -21,4 +36,6 @@ class Solution:
             temp = curr.next
             curr.next = prev
             prev = curr
-            cur
+            curr = temp
+
+        return prev
