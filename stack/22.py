7963ca1633c5a103e80f7472dfa64f42da5736a4
SaiSamarth123 <sai003sam@gmail.com>
2024-10-30 16:01:16 -0500

    problem 15

--- stack/22.py
+++ stack/22.py
@@ -3,6 +3,9 @@
 # Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 
 
+from typing import List
+
+
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
 
